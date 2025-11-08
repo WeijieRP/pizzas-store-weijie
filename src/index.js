fe818a623103a490983567d1b58c4e94a5dbd55c
weijie <hooiweijie60@gmail.com>
2025-11-08 11:57:46 +0800

    fix: remove unused imports to satisfy CRA CI build

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Header from "./Component/Header";
 import Menu from "./Component/Menu";
 import PizzaMenu from "./Pizza";     // keep only this (remove 'Pizza')
 import Footer from "./Component/Footer";
-import ThemeFab from "./ThemeFab";   // if you added the dark-mode button
+import ThemeFab from "./Component/ThemeFab";   // if you added the dark-mode button
 
 const pizzaItems = [
   {
